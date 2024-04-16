dd6d2223588e6e3a269ed648736ef3b03b0f9924
pooja-404 <pk3924792@gmail.com>
2024-04-16 10:02:11 +0530

    commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -16,6 +16,7 @@ import img14 from "../assets/images/img14.jpeg";
 import vite from "../assets/images/vite.png";
 import img15 from "../assets/images/img15.png";
 import img16 from "../assets/images/img16.png";
+import img17 from "../assets/images/img17.png";
 
 
 const Work = () => {
@@ -873,7 +874,7 @@ const Work = () => {
                                 <div className="w-3/5 px-3">
                                     <div className="flex pb-3 items-center">
                                         <p className="text-black pr-1 font-bold text-3xl font-poppins ">
-                                            Noble-Mind
+                                        CupidAI
                                         </p>
                                         <p className="text-black  text-xl font-poppins font-medium  ">
                                             (Date of submission : 11-04-2024)
@@ -899,6 +900,54 @@ const Work = () => {
                                                     target="blank"
                                                 >
                                                     https://cupid-kappa.vercel.app/
+                                                </a>
+                                            </span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap items-center flex-row -mx-3 pt-6">
+                                <div className="w-2/5 px-3">
+                                    <div className="flex">
+                                        <p className="text-black pr-1 font-semibold text-2xl font-poppins pb-3">
+                                            3.
+                                        </p>
+                                        <img
+                                            src={img17}
+                                            alt="1"
+                                            className="w-full max-h-[200px] h-full max-w-[350px]"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="w-3/5 px-3">
+                                    <div className="flex pb-3 items-center">
+                                        <p className="text-black pr-1 font-bold text-3xl font-poppins ">
+                                          Email.js
+                                        </p>
+                                        <p className="text-black  text-xl font-poppins font-medium  ">
+                                            (Date of submission : 15-04-2024)
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p className="text-black text-xl font-poppins font-medium  ">
+                                            Github-Link :{" "}
+                                            <span className="ms-3 border-b-2 border-black hover:text-blue-700">
+                                                <a
+                                                    href="https://github.com/pooja-404/email"
+                                                    target="blank"
+                                                >
+                                                    https://github.com/pooja-404/cupid
+                                                </a>
+                                            </span>
+                                        </p>
+                                        <p className="text-black text-xl font-poppins font-medium  ">
+                                            Live-Link :{" "}
+                                            <span className="ms-3 border-b-2 border-black hover:text-blue-700">
+                                                <a
+                                                    href="https://email-teal-three.vercel.app/"
+                                                    target="blank"
+                                                >
+                                                    https://email-teal-three.vercel.app/
                                                 </a>
                                             </span>
                                         </p>
